32ee58d329810b4bee31920022f5544662922044
Claeb101 <calebarulandu@gmail.com>
2023-08-04 16:58:43 -0400

    countdown

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,20 @@
-const startDate = new Date("2023-07-31")
+const startDate = new Date("2023-07-31T04:00:00.000Z")
 export const getDay = () => {
-  const now = new Date()
-  const days = Math.floor((now.getTime()-startDate.getTime())/(1000*60*60*24))
-  return days
+  return daysBetween(startDate, new Date())
+}
+
+export const getNextDayDate = () => {
+  const newDate = new Date(startDate.valueOf())
+  newDate.setDate(newDate.getDate()+getDay()+1)
+  return newDate
+}
+
+function treat(date: any) {
+  const result = new Date(date);
+  // result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
+  return result;
+}
+
+function daysBetween(startDate: any, endDate: any) {
+  return Math.floor((treat(endDate).valueOf() - treat(startDate).valueOf()) / (1000 * 60 * 60 * 24));
 }
--- src/routes/launcher.svelte
+++ src/routes/launcher.svelte
@@ -1,44 +1,58 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { getDay } from '@/lib';
+	import { getDay, getNextDayDate } from '@/lib';
+	import { time } from './stores';
 
-	let boards = 1, guesses = 6, sequential = false, day = getDay(), daily = true;
+	let boards = 1,
+		guesses = 6,
+		sequential = false,
+		day = getDay(),
+		daily = true;
+	$: timeToDaily = getNextDayDate().valueOf() - $time.valueOf();
+	$: hoursToDaily = Math.floor((timeToDaily / (1000 * 60 * 60)) % 24);
+	$: minsToDaily = Math.floor(timeToDaily / (1000 * 60) / 60);
+	$: secondsToDaily = Math.floor((timeToDaily / 1000) % 60);
+	const format = (x: number) => {
+		return x < 10 ? '0' + x : x;
+	};
 
 	const presets = [
-		{ name: 'wordle', boards: 1, guesses: 6},
-		{ name: 'quordle', boards: 4, guesses: 9},
+		{ name: 'wordle', boards: 1, guesses: 6 },
+		{ name: 'quordle', boards: 4, guesses: 9 },
 		{ name: 'octordle', boards: 8, guesses: 13 },
-		{ name: 'sedecordle', boards: 16, guesses: 21},
+		{ name: 'sedecordle', boards: 16, guesses: 21 }
 	];
-	let presetInd = 0
+	let presetInd = 0;
 	const applyPreset = (i: number) => {
 		boards = presets[i].boards ?? boards;
 		guesses = presets[i].guesses ?? guesses;
+	};
+	$: {
+		applyPreset(presetInd);
 	}
-	$: { applyPreset(presetInd) }
 
 	const play = async () => {
 		const res = await (
 			await fetch('/api/game', {
 				method: 'POST',
-				body: JSON.stringify({boards, guesses, sequential, day, daily})
+				body: JSON.stringify({ boards, guesses, sequential, day, daily })
 			})
 		).json();
 		goto('/play?' + new URLSearchParams({ id: res.id }));
 	};
 </script>
 
-<div class="mb-2 text-xl flex items-center justify-center">
-	<p>Ever wanted to play </p>
+<div class="mb-2 text-xl flex flex-wrap items-center justify-center">
+	<p>Ever wanted to play</p>
 	<select
 		class="mx-2 block bg-background border-solid border-2 border-primary rounded-md p-1"
 		bind:value={presetInd}
 	>
-		{#each presets as p,i}
+		{#each presets as p, i}
 			<option value={i}>{p.name}</option>
 		{/each}
 	</select>
-	 <p>but a bit different? Join the club.</p>
+	<p>but a bit different? Join the club.</p>
 </div>
 <div
 	class="mx-auto mt-2 w-fit flex flex-col sm:flex-row items-center justify-center border border-primary border-solid p-2 rounded-md bg-accent bg-opacity-5"
@@ -75,3 +89,4 @@
 		{@html '->'}
 	</button>
 </div>
+<p class="mt-2">Next daily in {format(hoursToDaily)} hrs {format(minsToDaily)} mins {format(secondsToDaily)}s</p>
